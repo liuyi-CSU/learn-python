0919d5ade776de56227ebf14e6c437f90ad556bf
liuyi <408827065@qq.com>
2017-09-17 17:21:59 +0800

    初始化 第一天 题目

---
+++ meijun/day001/test003.py
@@ -0,0 +1,36 @@
+"""
+
+明明想在学校中请一些同学一起做一项问卷调查，为了实验的客观性，他先用计算机生成了N个1到1000之间的随机整数（N≤1000），
+对于其中重复的数字，只保留一个，把其余相同的数去掉，不同的数对应着不同的学生的学号。然后再把这些数从小到大排序，
+按照排好的顺序去找同学做调查。请你协助明明完成“去重”与“排序”的工作。
+
+
+Input Param
+     n               输入随机数的个数
+ inputArray      n个随机整数组成的数组
+
+Return Value
+     OutputArray    输出处理后的随机整数
+
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+def random_int_list(start, stop, length):
+    start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
+    random_list = []
+    for i in range(length):
+        random_list.append(random.randint(start, stop))
+    return random_list
+
+print("请输入随机数的个数 n:")
+n = input()
+
+r = random_int_list(1, 10001, int(n))
+print(r)
+
+print(sorted(r))
+
